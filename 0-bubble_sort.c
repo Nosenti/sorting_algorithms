a979881eb8dad0f5bd142ec8e4c000627c986ff7
Nosenti <inno100os@gmail.com>
2024-03-04 11:41:07 +0200

    solving bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,28 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers using bubble sort algorithm
- * @array: Array of integers
- * @size: size of the array
- * Return: Nothing - just sort
+* bubble_sort - Sorts an array of integers using bubble sort algorithm
+* @array: Array of integers
+* @size: size of the array
+* Return: Nothing - just sort
 */
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, temp;
+	size_t i, j, temp;
 
-    for(i = 0; i < size - 1; i++){
-        for(j = i + 1; j < size; i++){
-            if (array[i] < array[j])
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-    }
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] < array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
 }
